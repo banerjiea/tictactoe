4bff0d5fdca991c419b1c8d804a60d80d61e2406
banerjiea <soubhik.hzs@gmail.com>
2021-06-01 14:29:26 +0530

    all rights reserved

---
+++ src/components/Square.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Square = ({ value, onClick, isWinningSquare }) => {
+  return (
+    <button
+      type="button"
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
